a5f37ce9acad6a122711824215bc575270fd6901
Dr. Sudip Chakraborty <38934168+sudipchakraborty@users.noreply.github.com>
2025-08-25 22:46:46 +0530

    GenAI process flow document added

--- app.py
+++ app.py
@@ -3,6 +3,11 @@
 # load_dotenv()  # load all the environment variables
 # text loader > text splitter > embedding > vectorDB > retriever >
 
+ # main.py
 
-   sss
+def main():
+    print("Hello, World! 🎉")
+    print("This is your Python project running inside venv.")
 
+if __name__ == "__main__":
+    main()
